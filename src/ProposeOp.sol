ea46844243acd6adf5db02886affa4715700694b
Kai Hiroi <kai.hiroi@pm.me>
2024-01-11 12:12:41 +0900

    Update: ProposeOp, make storage logic outside

--- src/ProposeOp.sol
+++ src/ProposeOp.sol
@@ -1,29 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Test } from "forge-std/Test.sol";
-import { console2 } from "forge-std/console2.sol";
-
+import {StorageLib} from "./StorageLib.sol";
 
 contract ProposeOp {
-    /// @custom:storage-location erc7201:ucstest.proposeop.proposals
-    struct ProposeOpStorage {
-        mapping(uint => string) proposals;
-        uint nextProposalId;
-    }
-
-    // keccak256(abi.encode(uint256(keccak256("ucstest.proposeop.proposals")) - 1)) & ~bytes32(uint256(0xff));
-    bytes32 private constant PROPOSALS_STORAGE_LOCATION =
-        0x5807a52450ab8f738d6948632d619254c674252efb511a8ef74d8ec2510c9b00;
-
-    function _getStorage() private pure returns (ProposeOpStorage storage $) {
-        assembly {
-            $.slot := PROPOSALS_STORAGE_LOCATION
-        }
-    }
-
     function propose(string calldata _proposalText) external returns (uint proposalId) {
-        ProposeOpStorage storage $ = _getStorage();
+        StorageLib.ProposeOpStorage storage $ = StorageLib.$Proposals();
         $.nextProposalId++;
         proposalId = $.nextProposalId;
         $.proposals[proposalId] = _proposalText;
