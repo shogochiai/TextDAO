8ef90714e355cb2a42c6e8c739b07a3d496bf282
shogochiai <shogo.ochiai@protonmail.com>
2024-01-12 16:17:27 +0900

    replace image, remove console.log

--- src/ExecuteOp.sol
+++ src/ExecuteOp.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.13;
 
 import { StorageLib } from "./StorageLib.sol";
-import { console2 } from "forge-std/console2.sol";
 
 contract ExecuteOp {
     function execute(uint pid) external returns (bool) {
         StorageLib.ProposeOpStorage storage $ = StorageLib.$Proposals();
         StorageLib.Proposal storage p = $.proposals[pid];
-        console2.logUint(p.yay);
-        console2.logUint(p.quorum);
         if (p.yay >= p.quorum && p.yay > p.nay) {
           for (uint i; i < p.commands.length; i++) {
              StorageLib.Command memory cmd = p.commands[i];
