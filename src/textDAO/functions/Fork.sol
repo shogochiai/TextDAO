dfa5316d34b15f5c536976b28dc217a81fcebd79
yusk <tdfagamdb@yahoo.co.jp>
2024-06-09 15:09:39 +0900

    add event HeaderForked & CommandForked

--- src/textDAO/functions/Fork.sol
+++ src/textDAO/functions/Fork.sol
@@ -6,6 +6,9 @@ import { Schema } from "bundle/textdao/storages/Schema.sol";
 import { Types } from "bundle/textdao/storages/Types.sol";
 
 contract Fork {
+    event HeaderForked(uint pid, uint headerId, uint currentScore, bytes32 metadataURI, uint[] tagIds);
+    event CommandForked(uint pid, uint cmdId, uint currentScore, string func, bytes abiParams);
+
     function fork(uint pid, Types.ProposalArg calldata _p) external onlyReps(pid) returns (uint forkId) {
         Schema.ProposeStorage storage $ = Storage.$Proposals();
         Schema.Proposal storage $p = $.proposals[pid];
@@ -15,6 +18,14 @@ contract Fork {
         }
         if (_p.cmd.actions.length > 0) {
             $p.cmds.push(_p.cmd);
+        }
+        if (_p.header.metadataURI.length > 0) {
+            emit HeaderForked(pid, _p.header.id, _p.header.currentScore, _p.header.metadataURI, _p.header.tagIds);
+        }
+        if (_p.cmd.actions.length > 0) {
+            for (uint i; i < _p.cmd.actions.length; i++) {
+                emit CommandProposed(pid, _p.cmd.id, _p.cmd.currentScore, _p.cmd.actions[i].func, _p.cmd.actions[i].abiParams);
+            }
         }
         // Note: Shadow(sender, timestamp)
     }
