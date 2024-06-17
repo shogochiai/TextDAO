de95355b6f122cfb528b60c54b2d915f56d3990e
yusk <tdfagamdb@yahoo.co.jp>
2024-06-17 22:07:51 +0900

    rename Voted event

--- src/textDAO/functions/Vote.sol
+++ src/textDAO/functions/Vote.sol
@@ -6,8 +6,8 @@ import { Storage } from "bundle/textdao/storages/Storage.sol";
 import { Schema } from "bundle/textdao/storages/Schema.sol";
 
 contract Vote {
-    event HeaderVoted(uint pid, uint headerId, uint currentScore);
-    event CmdVoted(uint pid, uint cmdId, uint currentScore);
+    event HeaderScored(uint pid, uint headerId, uint currentScore);
+    event CmdScored(uint pid, uint cmdId, uint currentScore);
 
     function voteHeaders(uint pid, uint[3] calldata headerIds) external returns (bool) {
         Schema.ProposeStorage storage $ = Storage.$Proposals();
@@ -17,19 +17,19 @@ contract Vote {
 
         if ($p.headers[0].id == headerIds[0]) {
             $p.headers[headerIds[0]].currentScore += 3;
-            emit HeaderVoted(pid, headerIds[0], $p.headers[headerIds[0]].currentScore);
+            emit HeaderScored(pid, headerIds[0], $p.headers[headerIds[0]].currentScore);
         } else if ($p.headers[1].id == headerIds[0]) {
             $p.headers[headerIds[0]].currentScore += 3;
             $p.headers[headerIds[1]].currentScore += 2;
-            emit HeaderVoted(pid, headerIds[0], $p.headers[headerIds[0]].currentScore);
-            emit HeaderVoted(pid, headerIds[1], $p.headers[headerIds[1]].currentScore);
+            emit HeaderScored(pid, headerIds[0], $p.headers[headerIds[0]].currentScore);
+            emit HeaderScored(pid, headerIds[1], $p.headers[headerIds[1]].currentScore);
         } else {
             $p.headers[headerIds[0]].currentScore += 3;
             $p.headers[headerIds[1]].currentScore += 2;
             $p.headers[headerIds[2]].currentScore += 1;
-            emit HeaderVoted(pid, headerIds[0], $p.headers[headerIds[0]].currentScore);
-            emit HeaderVoted(pid, headerIds[1], $p.headers[headerIds[1]].currentScore);
-            emit HeaderVoted(pid, headerIds[2], $p.headers[headerIds[2]].currentScore);
+            emit HeaderScored(pid, headerIds[0], $p.headers[headerIds[0]].currentScore);
+            emit HeaderScored(pid, headerIds[1], $p.headers[headerIds[1]].currentScore);
+            emit HeaderScored(pid, headerIds[2], $p.headers[headerIds[2]].currentScore);
         }
     }
     function voteCmds(uint pid, uint[3] calldata cmdIds) external returns (bool) {
@@ -40,19 +40,19 @@ contract Vote {
 
         if ($p.cmds[0].id == cmdIds[0]) {
             $p.cmds[cmdIds[0]].currentScore += 3;
-            emit CmdVoted(pid, cmdIds[0], $p.cmds[cmdIds[0]].currentScore);
+            emit CmdScored(pid, cmdIds[0], $p.cmds[cmdIds[0]].currentScore);
         } else if ($p.cmds[1].id == cmdIds[0]) {
             $p.cmds[cmdIds[0]].currentScore += 3;
             $p.cmds[cmdIds[1]].currentScore += 2;
-            emit CmdVoted(pid, cmdIds[0], $p.cmds[cmdIds[0]].currentScore);
-            emit CmdVoted(pid, cmdIds[1], $p.cmds[cmdIds[1]].currentScore);
+            emit CmdScored(pid, cmdIds[0], $p.cmds[cmdIds[0]].currentScore);
+            emit CmdScored(pid, cmdIds[1], $p.cmds[cmdIds[1]].currentScore);
         } else {
             $p.cmds[cmdIds[0]].currentScore += 3;
             $p.cmds[cmdIds[1]].currentScore += 2;
             $p.cmds[cmdIds[2]].currentScore += 1;
-            emit CmdVoted(pid, cmdIds[0], $p.cmds[cmdIds[0]].currentScore);
-            emit CmdVoted(pid, cmdIds[1], $p.cmds[cmdIds[1]].currentScore);
-            emit CmdVoted(pid, cmdIds[2], $p.cmds[cmdIds[2]].currentScore);
+            emit CmdScored(pid, cmdIds[0], $p.cmds[cmdIds[0]].currentScore);
+            emit CmdScored(pid, cmdIds[1], $p.cmds[cmdIds[1]].currentScore);
+            emit CmdScored(pid, cmdIds[2], $p.cmds[cmdIds[2]].currentScore);
         }
     }
 }
